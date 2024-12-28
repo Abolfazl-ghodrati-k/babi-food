72f88a2be0d88a1674a565b4b5478fdfdcf0f599
Ghodrati <Ghodrati.Abolfazl@tapsi.shop>
2024-12-28 10:12:41 +0330

    feat: initialized project base

---
+++ apps/users/src/dto/user.dto.ts
@@ -0,0 +1,38 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+@InputType()
+export class RegisterDto {
+  @Field()
+  @IsNotEmpty({ message: 'Name is Required' })
+  @IsString({ message: 'Name must need to be one string' })
+  name: string;
+
+  @Field()
+  @IsNotEmpty({ message: 'Password is required.' })
+  @MinLength(8, { message: '.Password must be at least 8 characters.' })
+  password: string;
+
+  @Field()
+  @IsNotEmpty({ message: 'Email is required.' })
+  @IsEmail({}, { message: 'Email is Invalid.' })
+  email: string;
+  
+  @Field()
+  @IsNotEmpty({ message: 'PhoneNumber is required.' })
+  @IsEmail({}, { message: 'PhoneNumber is Invalid.' })
+  phone_number: number;
+}
+
+@InputType()
+export class LoginDto {
+  @Field()
+  @IsNotEmpty({ message: 'Password is required.' })
+  password: string;
+
+  @Field()
+  @IsNotEmpty({ message: 'Email is required.' })
+  @IsEmail({}, { message: 'Email is Invalid.' })
+  email: string;
+}
+
---
+++ apps/users/src/users.service.ts
@@ -0,0 +1,88 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto, RegisterDto } from './dto/user.dto';
+import { PrismaService } from '../../../prisma/Prisma.service';
+import { Response } from 'express';
+import { hash } from 'bcrypt';
+
+interface UserData {
+  name: string;
+  email: string;
+  password: string;
+  phone_number: number;
+}
+
+@Injectable()
+export class UsersService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async register(registerDto: RegisterDto, response: Response) {
+    const { email, name, password, phone_number } = registerDto;
+
+    const isEmailExists = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (isEmailExists) {
+      throw new BadRequestException('User already exist with that email');
+    }
+
+    const isPhoneNumberExists = await this.prisma.user.findUnique({
+      where: {
+        phone_number,
+      },
+    });
+
+    if (isPhoneNumberExists) {
+      throw new BadRequestException(
+        'User already exist with that phone number!',
+      );
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const user = {
+      name,
+      email,
+      password: hashedPassword,
+      phone_number,
+    };
+
+    const activationToken = await this.createActivationToken(user);
+
+    const activationCode = activationToken.activationCode;
+
+    console.log(activationCode);
+
+    return { user, response };
+  }
+
+  async createActivationToken(user: UserData) {
+    const activationCode = Math.floor(1000 + Math.random() * 9000).toString();
+
+    const token = this.jwtService.sign(
+      { user, activationCode },
+      { secret: this.configService.get('ACTIVATION_SECRET'), expiresIn: '5m' },
+    );
+
+    return { token, activationCode };
+  }
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    const user = { email, password };
+
+    return user;
+  }
+
+  async getUsers() {
+    return await this.prisma.user.findMany();
+  }
+}
